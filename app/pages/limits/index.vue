e0d1e19396dd23b09c1dc0e13fdf61cd8ff38c8b
Flossy Yuri <emerson.yur@gmail.com>
2025-10-14 02:49:50 +0200

    fix: update limit list

--- app/pages/limits/index.vue
+++ app/pages/limits/index.vue
@@ -76,7 +76,7 @@
     <!-- Limits Grid -->
     <div
       v-if="limitsStore.isLoading"
-      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6"
+      class="grid grid-cols-1 md:grid-cols-2 gap-6"
     >
       <div v-for="i in 6" :key="i" class="animate-pulse">
         <div class="bg-gray-200 dark:bg-gray-700 rounded-lg h-32"></div>
@@ -96,7 +96,7 @@
       </Button>
     </div>
 
-    <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div v-else class="grid grid-cols-1 md:grid-cols-2 gap-6">
       <LimitCard
         v-for="limit in filteredLimits"
         :key="limit.limit_id"
